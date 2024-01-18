c944b4dcff7692b64cf9920acd90b17c28171141
ohara19 <oharatottenham@yahoo.com>
2024-01-18 05:19:21 -0800

    check a list wether is cycle or not

--- 0x00-python-hello_world/10-check_cycle.c
+++ 0x00-python-hello_world/10-check_cycle.c
@@ -1,27 +1,26 @@
 #include "lists.h"
 
 /**
- * check a cycle  a function that check if a list has a cycle
- * the linked list
- * return 1 if there is a cycle and 0 if not
+ * check_cycle - checks if a linked list contains a cycle
+ * @list: linked list to check
+ *
+ * Return: 1 if the list has a cycle, 0 if it doesn't
  */
-int check_cycle(listint_t *list);
+int check_cycle(listint_t *list)
 {
-	listint_t *fast *slow;
+	listint_t *slow = list;
+	listint_t *fast = list;
 
-	if (!list || !list->next)
+	if (!list)
 		return (0);
-	fast = list->next->next;
-	slow = list;
-	
-	while (slow != NULL && fast != NULL && fast-> != NULL)
+
+	while (slow && fast && fast->next)
 	{
 		slow = slow->next;
 		fast = fast->next->next;
 		if (slow == fast)
-		{	
-		return (1);
-		}
+			return (1);
 	}
+
 	return (0);
 }
