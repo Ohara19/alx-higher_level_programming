117851c2df3e83493e3ebb8667f8df2fbbddd50c
ohara19 <oharatottenham@yahoo.com>
2024-02-05 05:54:45 -0800

    print an interger from element

--- 0x05-python-exceptions/2-safe_print_list_integers.py
+++ 0x05-python-exceptions/2-safe_print_list_integers.py
@@ -5,7 +5,7 @@ def safe_print_list_integers(my_list=[], x=0):
     for items in range(x):
         try:
             print("{:d}".format(my_list[items]), end='')
-        except (ValueError, TypeError, IndexError):
+        except (ValueError, TypeError):
             pass
         print()
         return num
