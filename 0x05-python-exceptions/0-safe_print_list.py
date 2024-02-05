b4d119f1cfbe0564a52ee06f9b8cbd9fa4b979c1
ohara19 <oharatottenham@yahoo.com>
2024-02-05 03:38:19 -0800

    print x in range of list element

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
+
 def safe_print_list(my_list=[], x=0):
     num = 0
     try:
-        for item in the range of (x):
+        for item in  range (x):
             print("{}".format(my_list[item]), end'')
             num += 1
     except IndexError:
