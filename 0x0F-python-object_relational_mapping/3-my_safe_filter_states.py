ea78a6eacc711d13dcb14378f48d9ba04db0bfd6
ohara19 <oharatottenham@yahoo.com>
2024-07-04 07:26:43 -0700

    SQL Injection

--- 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
+++ 0x0F-python-object_relational_mapping/3-my_safe_filter_states.py
@@ -2,11 +2,23 @@
 """
 Displays all values in the states table of
 hbtn_0e_0_usa where name matches the argument.
+(Safe from SQL Injection)
 """
 
 if __name__ == '__main__':
     from sys import argv
     import MySQLdb as mysql
+    import re
+
+    if (len(argv) != 5):
+        print('Use: username, password, database name, state name')
+        exit(1)
+
+    searched = ' '.join(argv[4].split())
+
+    if (re.search('^[a-zA-Z ]+$', searched) is None):
+        print('Enter a valid name state (example: Arizona)')
+        exit(1)
 
     try:
         db = mysql.connect(host='localhost', port=3306, user=argv[1],
@@ -15,12 +27,10 @@ if __name__ == '__main__':
         print('Failed to connect to the database')
         exit(0)
 
-    searched = argv[4]
-
     cursor = db.cursor()
 
-    cursor.execute("SELECT * FROM states WHERE name = BINARY '{:s}' \
-                    ORDER BY id ASC;".format(searched))
+    cursor.execute("SELECT * FROM states \
+                    WHERE name = '{:s}' ORDER BY id ASC;".format(searched))
 
     result_query = cursor.fetchall()
 
